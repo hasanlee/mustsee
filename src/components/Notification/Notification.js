8cf5e6083c180f2ef3f1a3ff66f13b9cb16e5108
Hasanli Hasan <hasan@hasanli.info>
2023-01-10 00:37:02 +0400

    animation for Notification toast

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,24 +1,36 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 
-export default function Notification({ badge, text }) {
+export default function Notification({
+  badge = "NEW",
+  text,
+  type = "default",
+}) {
   return (
-    <div className='flex w-screen justify-center items-center mt-2 absolute '>
-      <div
-        className='p-2 bg-black dark:bg-slate-700 items-center text-slate-100 leading-none rounded-full flex lg:inline-flex'
-        role='alert'
-      >
-        <span className='flex rounded-full bg-white text-black dark:bg-slate-200 dark:text-slate-900 uppercase px-2 py-1 text-xs font-bold mr-3'>
-          {badge || "NEW"}
-        </span>
-        <span className='font-semibold mr-2 text-left flex-auto'>{text}</span>
-        <svg
-          className='fill-current opacity-75 h-4 w-4'
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 20 20'
+    <Fade>
+      <div className='flex w-screen justify-center items-center mt-2 absolute'>
+        <div
+          className='p-2 bg-black dark:bg-slate-700 items-center text-slate-100 leading-none rounded-full flex lg:inline-flex '
+          role='alert'
         >
-          <path d='M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z' />
-        </svg>
+          <span
+            className={
+              "flex rounded-full uppercase px-2 py-1 text-xs font-bold mr-3 " +
+              type
+            }
+          >
+            {badge}
+          </span>
+          <span className='font-semibold mr-2 text-left flex-auto'>{text}</span>
+          <svg
+            className='fill-current opacity-75 h-4 w-4'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 20 20'
+          >
+            <path d='M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z' />
+          </svg>
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    keyframes: {
+      slideInTop: {
+        "0%": {  transform: "translateY(-1000px)", opacity:0 },
+        "100%": { transform: "translateY(0)", opacity: 1 },
+      },
+      slideOutTop: {
+        "0%": { transform: "translateY(0)", opacity: 1 },
+        "100%": { transform: "translateY(-1000px)", opacity: 0 },
+      },
+    },
+    animation: {
+      slideInTop: "slideInTop 0.9s ease-in-out both",
+      slideOutTop: "slideOutTop 0.9s ease-in-out both",
+    },
   },
   plugins: [],
 };
