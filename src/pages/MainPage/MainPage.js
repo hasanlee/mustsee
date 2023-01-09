ad12e83eb34d0e87e3cc87a394ed0f873519dd53
Hasanli Hasan <hasan@hasanli.info>
2023-01-09 20:32:48 +0400

    mobile friendly )

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -7,16 +7,16 @@ const MainPage = () => {
   return (
     <div>
       <Header />
-      <main className='flex py-8 px-10 w-full'>
-        <section className='w-3/5'>
-          <div className='w-full px-8'>
+      <main className='flex py-8 px-10 w-full max-md:flex-col gap-3'>
+        <section className='w-3/5 max-md:w-full'>
+          <div className='w-full'>
             <SearchBox />
           </div>
           <div className='p-8'>
             <Movies />
           </div>
         </section>
-        <aside className='w-2/5'>
+        <aside className='w-2/5 max-md:w-full'>
           <Favorites />
         </aside>
       </main>
